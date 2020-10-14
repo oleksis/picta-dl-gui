1528b588b4df880e4925b8e9350bc276288f6949
oleksis <oleksis.fraga@gmail.com>
2020-10-14 01:51:45 -0400

    Change mainwindow.ui and configuration.ui

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include "mainwindow.h"
 #include "RunGuard.h"
-#include <QObject>
 #include <QApplication>
 
 
@@ -15,7 +14,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setApplicationName("Picta-dl_GUI");
     MainWindow w;
-    w.setWindowFlags(Qt::MSWindowsFixedSizeDialogHint);
     w.show();
 
     return a.exec();
