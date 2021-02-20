f7f4e02c7e3ee3aed1cff666d3ffec713e91b4e9
oleksis <oleksis.fraga@gmail.com>
2021-02-20 00:10:10 -0500

    Add dependencies to installer Offline

--- make_installer.sh
+++ make_installer.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# Crear un Installador Offline con Qt Installer Framework [Windows]
+# Crear un Installador Offline con Qt Installer Framework using MinGW64 [Windows]
 #
 export DEST_DIR=./deployment/windows/packages/cu.pictadl.gui/data
 export PACKAGE_DIR=./deployment/windows
 
 echo "Cleaning ..."
+rm -rf ./release/*
 rm -rf ${DEST_DIR}/*
 
 echo "Building Picta-dl_GUI ..."
@@ -14,8 +15,31 @@ make -j2
 # echo "Create output directory if it does not exist"
 # mkdir -p "${DEST_DIR}/tools"
 
-echo "Copying the final executables to: ${DEST_DIR}"
+echo "Copy the final executables and dependencies to: ${DEST_DIR}"
+echo "Copying dependencies ..."
 cp -f "./release/Picta-dl_GUI.exe" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libwinpthread-1.dll" ${DEST_DIR}
+cp -f $MINGW_PREFIX/bin/libgcc_*-1.dll ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libstdc++-6.dll" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libharfbuzz-0.dll" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libpng16-16.dll" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/zlib1.dll" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libgraphite2.dll" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libfreetype-6.dll" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libglib-2.0-0.dll" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libbrotlidec.dll" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libbz2-1.dll" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libintl-8.dll" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libpcre-1.dll" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libdouble-conversion.dll" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libicuin67.dll" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libicuuc67.dll" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libpcre2-16-0.dll" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libzstd.dll" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libicudt67.dll" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libbrotlicommon.dll" ${DEST_DIR}
+cp -f "$MINGW_PREFIX/bin/libiconv-2.dll" ${DEST_DIR}
+echo "Copying executables ..."
 cp -f "./Resources/picta-dl.exe" ${DEST_DIR}
 cp -f "./Resources/ffmpeg.exe" ${DEST_DIR}
 
